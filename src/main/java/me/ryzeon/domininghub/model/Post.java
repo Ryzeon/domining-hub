40e9fec6530508d2708f1185d1248543e2dfd347
Alex Avila <alex.ramon2004@gmail.com>
2024-06-06 18:59:53 -0500

    feat: created post entity

---
+++ src/main/java/me/ryzeon/domininghub/model/Post.java
@@ -0,0 +1,38 @@
+package me.ryzeon.domininghub.model;
+
+import me.ryzeon.domininghub.shared.storage.model.File;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Alex Avila Asto - A.K.A (Ryzeon)
+ * Project: domining-hub
+ * Date: 6/6/24 @ 18:51
+ */
+@Document
+public class Post {
+
+    @Id
+    private String id;
+
+    @DBRef
+    private User user;
+
+    private String title;
+    private String content;
+
+    @CreatedDate
+    private Date createdAt;
+
+    private String company;
+
+    @DBRef
+    private List<File> files;
+
+    private int likes;
+}
