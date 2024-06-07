1dad4797667c0838166e9de086acba93c8e99f33
Alex Avila <alex.ramon2004@gmail.com>
2024-06-06 19:00:11 -0500

    feat: created comments for post.

---
+++ src/main/java/me/ryzeon/domininghub/model/Comments.java
@@ -0,0 +1,31 @@
+package me.ryzeon.domininghub.model;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+/**
+ * Created by Alex Avila Asto - A.K.A (Ryzeon)
+ * Project: domining-hub
+ * Date: 6/6/24 @ 18:56
+ */
+@Document
+public class Comments {
+
+    @Id
+    private String id;
+
+    @DBRef
+    private User user;
+
+    @DBRef
+    private Post post;
+
+    private String content;
+
+    @CreatedDate
+    private Date createdAt;
+}
